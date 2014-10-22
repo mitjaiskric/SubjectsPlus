cd80587cf15b4e0d9fdbf7df63a7b9185412aa53
James Little <j.little@miami.edu>
2014-10-22 10:06:50 -0400

    Updating LibGuides importer to not allow duplicate titles

--- control/includes/import_libguides.php
+++ control/includes/import_libguides.php
@@ -11,21 +11,34 @@ use SubjectsPlus\Control\Querier;
 use SubjectsPlus\Control\LibGuidesImport;
 
 
-
-
-
 $libguides_importer = new LibGuidesImport;
-//$is_imported = $libguides_importer->guide_dupe($_POST['libguide'][0]);
-
-//echo $_POST['libguide'][0];
 
 
+// Set the guide id 
 $libguides_importer->setGuideID($_POST['libguide']);
 
+
+if ($libguides_importer->guide_imported()) {
+
+echo "This guide has already been imported.";
+
+} else {
+
+// Load all the links from the XML
 $libguides_xml = $libguides_importer->load_libguides_links_xml('libguides.xml');
+
+// Load the XML
 $libguides_xml = $libguides_importer->load_libguides_xml('libguides.xml');
 
+
+// Import the guides with the XML you just loaded
 $libguides_importer->import_libguides($libguides_xml);
+
+
+}
+
+
+
 
 
 
