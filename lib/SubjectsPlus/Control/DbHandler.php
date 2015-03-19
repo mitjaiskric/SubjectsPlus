e478f99797c5ad70d39876c74230dc2ad3a1b910
James Little <j.little@miami.edu>
2015-03-19 11:20:58 -0400

    Added condition for num alternate titles
    A more specific query to catch "Nineteen" in the num option

--- lib/SubjectsPlus/Control/DbHandler.php
+++ lib/SubjectsPlus/Control/DbHandler.php
@@ -9,12 +9,12 @@ namespace SubjectsPlus\Control;
 *   @todo  This just writes out the db results, not a proper object, I suppose.
 */
 ///////// Databases ///////////
-
-
+ 
+ 
 use SubjectsPlus\Control\Querier;
-
+ 
 class DbHandler {
-	
+
 	function writeTable($qualifier, $subject_id = '', $description_search = 0) {
 		
 		global $IconPath;
@@ -22,54 +22,51 @@ class DbHandler {
 
 		$db = new Querier;
 		// sanitize submission
-		
 		$subject_id = scrubData($subject_id);
-		
+
 		// Prepare conditions
 		$condition1 = "";
-		$condition2 = "";
-		$condition3 = "";
-		
+	
+                // $qualifier is the databases.php?letter= GET value	
+ 
 		switch ($qualifier) {
 		case "Num":
 			$condition1 = "WHERE left(title, 1)  REGEXP '[[:digit:]]+'";
-			$condition2 = "WHERE left(alternate_title, 1)  REGEXP '[[:digit:]]+'";
+	
 			break;
 		case "All":
 			$condition1 = "WHERE title != ''";
-			$condition2 = "WHERE alternate_title != ''";
+		
 			break;
 		case "bysub":
-
-			if (isset($subject_id)) {
+ if (isset($subject_id)) {
 				//get title ids in pluslets' resource token connected to subject
 				$lobjGuide = new Guide($subject_id);
 				$lobjTitleIds = $lobjGuide->getRelatedTitles();
-
+ 
 				$condition1 = "WHERE (subject_id = $subject_id";
 				$condition1 .= count($lobjTitleIds) > 0 ? "\nOR t.title_id IN (" . implode( ',', $lobjTitleIds) . ")" : "";
 				$condition1 .= ")";
 				$condition2 = "WHERE subject_id = $subject_id";
 			} else {
 				$condition1 = "WHERE title LIKE " . $db->quote("%" . $qualifier . "%");
-				$condition2 = "WHERE alternate_title LIKE " . $db->quote("%" . $qualifier . "%");
+				$condition2 = "WHERE alternate_title LIKE " . $db->quote("%" . $qualifier . "%") . "AND type = 'Subject'";
 			}
 			break;
 		case "bytype":
-
+ 
 			if (isset($_GET["type"])) {
-				$condition1 = "WHERE ctags LIKE " . $db->quote(scrubData($_GET["type"]));
-				$condition2 = "WHERE ctags LIKE " . $db->quote(scrubData($_GET["type"]));
-				$condition3 = "and alternate_title NOT NULL";
+				$condition1 = "WHERE ctags LIKE " . $db->quote(scrubData("%" . $_GET["type"] . "%"));
+			
 			}
-
+ 
 			break;
 		case "search":
 			$condition1 = "WHERE title LIKE " . $db->quote("%" . $qualifier . "%");
 			// If you uncomment the next line, it will search description field
 			$condition1 = "WHERE (title LIKE " . $db->quote("%" . $qualifier . "%") . " OR description LIKE " . $db->quote("%" . $qualifier . "%");
-			$condition2 = "WHERE alternate_title LIKE " . $db->quote("%" + $qualifier + "%");
-
+		
+ 
 			break;
 			
 		default:
@@ -78,7 +75,7 @@ class DbHandler {
 			if (strlen($qualifier) == 1) {
 				
 				// Is like the first letter
-				$condition1 = "WHERE title LIKE " . $db->quote($qualifier . "%");
+				$condition1 = "WHERE  left(title,1) = '$qualifier'";
 				
 			} else {
 				$condition1 = "WHERE title LIKE " . $db->quote("%" . $qualifier . "%");
@@ -86,15 +83,16 @@ class DbHandler {
 			
 			if ($description_search == 1) {
 				// If you uncomment the next line, it will search description field
-				$condition1 = "WHERE (title LIKE " . $db->quote("%" . $qualifier . "%") . " OR description LIKE " . $db->quote("%" . $qualifier . "%") . ")";
+			$condition1 = "WHERE (title LIKE " . $db->quote("%" . $qualifier . "%") . " OR description LIKE " . $db->quote("%" . $qualifier . "%") . ")";
 			}
-
-			$condition2 = "WHERE alternate_title LIKE " . $db->quote("%" + "J" + "%");
+ 
+		
 			
 		}
-		$first_letter = $qualifier;
 
-		$qualifier = $db->quote("%$qualifier%");
+                
+            	$first_letter = $db->quote("%$qualifier%");
+	
 		$main_title_query = "SELECT distinct left(t.title,1) as initial, t.title as newtitle, t.description, location, access_restrictions, t.title_id as this_record,eres_display, display_note, pre, citation_guide, ctags, helpguide
 			FROM title as t
 			INNER JOIN location_title as lt
@@ -110,7 +108,7 @@ class DbHandler {
 			$condition1
 			AND eres_display = 'Y'
 			ORDER BY newtitle";
-
+ 
 		$alternate_title_query = "SELECT distinct left(t.alternate_title,1) as initial, t.alternate_title as newtitle, t.description, location, access_restrictions, t.title_id as this_record,eres_display, display_note, pre, citation_guide, ctags, helpguide
 			FROM title as t
 			INNER JOIN location_title as lt
@@ -123,54 +121,76 @@ class DbHandler {
 			ON rk.title_id = t.title_id
 			INNER JOIN source as s
 			ON rk.source_id = s.source_id
-			WHERE alternate_title LIKE $qualifier
-			AND eres_display = 'Y'
+                                  
+			WHERE LEFT(t.alternate_title,1) = $first_letter
+		                             
+	                AND eres_display = 'Y'
 			ORDER BY newtitle";
-
-
+ 
+	$num_alt_title_query = "SELECT distinct LEFT(t.alternate_title,1) as initial, t.title as newtitle, t.description, location, access_restrictions, t.title_id as this_record,eres_display, display_note, pre, citation_guide, ctags, helpguide
+			FROM title as t
+			INNER JOIN location_title as lt
+			ON t.title_id = lt.title_id
+			INNER JOIN location as l
+			ON lt.location_id = l.location_id
+			INNER JOIN restrictions as r
+			ON l.access_restrictions = r.restrictions_id
+			INNER JOIN rank as rk
+			ON rk.title_id = t.title_id
+			INNER JOIN source as s
+			ON rk.source_id = s.source_id
+			WHERE left(alternate_title, 1) REGEXP '[[:digit:]]+'
+                        OR left(title, 1) REGEXP '[[:digit:]]+'
+	                AND eres_display = 'Y'
+			ORDER BY newtitle";
+ 
 		$main_titles = $db->query($main_title_query);
 		$alternate_titles = $db->query($alternate_title_query);
-		
-		// Merge the two results, alternate and main
-		$main_alternate_titles = array_merge($main_titles,$alternate_titles);
+		$num_titles = $db->query($num_alt_title_query); 
+            
+      
+                if ($qualifier == "Num") {
+                 
+                    $main_alternate_titles = $num_titles;
+
+                } else {
+                   
+                    $main_alternate_titles = array_merge_recursive($main_titles , $alternate_titles);
+                }
 		
 		// Check to see if the qualifier is not a single letter like "C", it could be "All" or "Audio"
 		if(strlen($first_letter) === 1) {
 			
-			// If it is a single letter, remove titles that don't start with that same letter
-		foreach ($main_alternate_titles as $index => $title) {
-			
-			$title_first_letter = substr($title[0], 0);
-			
-			if ($title_first_letter != $first_letter) {
+		// Remove results from array that don't have the currently selected initial
+		
+		foreach ($main_alternate_titles as $index => $value) {
+		
+			if ($value['initial'] != $first_letter) {
 				
 				unset($main_alternate_titles[$index]);
 				
-			}
-			
-			
+			} 
 		}
 		
 		}
 		
 		$num_rows = count($main_alternate_titles);
-
-		
+ 
 		if ($num_rows == 0) {
 			return "<div class=\"no_results\">" . _("Sorry, there are no results at this time.") . "</div>";
 		}
-
+ 
 		// prepare 	header
-		$items = "<table width=\"98%\" class=\"item_listing\">";
-
+		$items = "<table width=\"98%\" class=\"item_listing trackContainer\">";
+ 
 		$main_alternate_titlesow_count = 0;
 		$colour1 = "oddrow";
 		$colour2 = "evenrow";
-
+ 
 		foreach ($main_alternate_titles as $myrow) {
-
+ 
 			$main_alternate_titlesow_colour = ($main_alternate_titlesow_count % 2) ? $colour1 : $colour2;
-
+ 
 			$patterns = "/'|\"/";
 			$main_alternate_titleseplacements = "";
 			
@@ -184,9 +204,9 @@ class DbHandler {
 			$safe_title = trim(preg_replace($patterns, $main_alternate_titleseplacements, $item_title));
 			$blurb = $myrow["description"];
 			$bib_id = $myrow[5];
-
+ 
 			/// CHECK RESTRICTIONS ///
-
+ 
 			if (($myrow['4'] == 2) OR ($myrow['4'] == 3)) {
 				$url = $proxyURL . $myrow[3];
 				$main_alternate_titlesest_icons = "restricted";
@@ -197,12 +217,12 @@ class DbHandler {
 				$url = $myrow[3];
 				$main_alternate_titlesest_icons = ""; // if you want the unlocked icon to show, enter "unrestricted" here
 			}
-
+ 
 			$current_ctags = explode("|", $myrow["ctags"]);
-
+ 
 			// add our $main_alternate_titlesest_icons info to this array at the beginning
 			array_unshift($current_ctags, $main_alternate_titlesest_icons);
-
+ 
 			$icons = showIcons($current_ctags);
 			
 			/// Check for Help Guide ///
@@ -213,16 +233,16 @@ class DbHandler {
 			}
 			
 			//Check if there is a display note
-
+ 
 			if ($myrow["display_note"] == NULL) {
 				$display_note_text = "";
 			} else {
 				$display_note_text = "<br /><strong>" . _("Note:") . " </strong>" . $myrow['display_note'];
 			}
-
-
+ 
+ 
 			$bonus = "$blurb<br />";
-
+ 
 			if ($blurb != "") {
 				$information1 = "<span id=\"bib-$bib_id\" class=\"toggleLink curse_me\"><img src=\"$IconPath/information.png\" border=\"0\" alt=\"" . _("more information") . "\" title=\"" . _("more information") . "\" /></span>";
 				// This is new details link; you can use the one above if you prefer
@@ -231,18 +251,18 @@ class DbHandler {
 			} else {
 				$information = "";
 			}
-
+ 
 			$target = targetBlanker();    
-
+ 
 			$items .= self::generateLayout($main_alternate_titlesow_colour,$url,$target,$item_title,$information,$information1,$icons,$helpguide,$display_note_text,$bonus);
-
+ 
 			$main_alternate_titlesow_count++;
 		}
-
+ 
 		$items .= "</table>";
 		return $items;
 	}
-
+ 
 	function generateLayout($main_alternate_titlesow_colour,$url,$target,$item_title,$information,$information1,$icons,$helpguide,$display_note_text,$bonus) {
 		$onerow = "<tr class=\"zebra $main_alternate_titlesow_colour\" valign=\"top\">
 	<td><a href=\"$url\" $target>$item_title</a> $information <span class=\"db_icons\">$icons</span> $helpguide $display_note_text
@@ -254,55 +274,55 @@ class DbHandler {
 		<div class=\"list_bonus\">$bonus</div>
 	</td>  
 	</tr>";
-
+ 
 		return $onerow;
-
+ 
 	}
-
+ 
 	function displaySubjects() {
 	    $db = new Querier;	
-		$q = "SELECT subject, subject_id FROM subject WHERE active = '1' ORDER BY subject";
+		$q = "SELECT subject, subject_id FROM subject WHERE active = '1' AND type = 'Subject' ORDER BY subject";
 		$main_alternate_titles = $db->query($q);
-
+ 
 		// check row count for 0 returns
-
+ 
 		$num_rows = count($main_alternate_titles);
-
+ 
 		if ($num_rows == 0) {
 			return "<div class=\"no_results\">" . _("Sorry, there are no results at this time.") . "</div>";
 		}
-
+ 
 		// prepare 	header
 		$items = "<table width=\"98%\" class=\"item_listing\">";
-
+ 
 		$main_alternate_titlesow_count = 0;
 		$colour1 = "oddrow";
 		$colour2 = "evenrow";
-
-
+ 
+ 
 		foreach ($main_alternate_titles as $myrow) {
 			
 			$main_alternate_titlesow_colour = ($main_alternate_titlesow_count % 2) ? $colour1 : $colour2;
-
+ 
 			$items .= "
 	<tr class=\"zebra $main_alternate_titlesow_colour\" valign=\"top\">
 		<td><a href=\"databases.php?letter=bysub&subject_id=$myrow[1]\">$myrow[0]</a></td>
 	</tr>";
-
+ 
 			$main_alternate_titlesow_count++;
 		}
-
+ 
 		$items .= "</table>";
 		return $items;
 	}
-
+ 
 	function displayTypes() {
 		global $all_ctags;
 		sort($all_ctags);
-
+ 
 		// prepare 	header
 		$items = "<table width=\"98%\" class=\"item_listing\">";
-
+ 
 		foreach ($all_ctags as $value) {
 			
 			$pretty_type = ucwords(preg_replace('/_/', ' ', $value));
@@ -314,13 +334,13 @@ class DbHandler {
 		$items .= "</table>";
 		return $items;
 	}
-
+ 
 	function searchFor($qualifier) {
 		
 	}
-
+ 
 	function deBug() {
 		print "Query:  " . $q;
 	}
-
+ 
 }
